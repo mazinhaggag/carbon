c5686da21b7100f6d6d619936eb4f30e68e4b1c5
Mazin Haggag <mazinhaggag@gmail.com>
2025-08-19 19:16:40 -0400

    a

--- decoders/dynamic-curve/src/instructions/evt_swap2_event.rs
+++ decoders/dynamic-curve/src/instructions/evt_swap2_event.rs
@@ -1,9 +1,18 @@
-
-
 use carbon_core::{borsh, CarbonDeserialize};
+
+use crate::types::{SwapParameters2, SwapResult2};
 
 
 #[derive(CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash)]
 #[carbon(discriminator = "0xe445a52e51cb9a1dbd4233a826507599")]
-pub struct EvtSwap2Event{
+pub struct EvtSwap2Event {
+    pub pool: solana_pubkey::Pubkey,
+    pub config: solana_pubkey::Pubkey,
+    pub trade_direction: u8,
+    pub has_referral: bool,
+    pub swap_parameters: SwapParameters2,
+    pub swap_result: SwapResult2,
+    pub quote_reserve_amount: u64,
+    pub migration_threshold: u64,
+    pub current_timestamp: u64,
 }
