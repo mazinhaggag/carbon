f0aaa51b42a0be39ce5195bee3c122508b6cfb06
Mazin Haggag <mazinhaggag@gmail.com>
2025-08-19 20:56:34 -0400

    a

--- decoders/dynamic-curve/src/instructions/evt_create_config_v2_event.rs
+++ decoders/dynamic-curve/src/instructions/evt_create_config_v2_event.rs
@@ -2,8 +2,14 @@
 
 use carbon_core::{borsh, CarbonDeserialize};
 
+use crate::types::ConfigParameters;
 
 #[derive(CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash)]
 #[carbon(discriminator = "0xe445a52e51cb9a1da34a42bb77c31a90")]
-pub struct EvtCreateConfigV2Event{
+pub struct EvtCreateConfigV2Event {
+    pub config: solana_pubkey::Pubkey,
+    pub quote_mint: solana_pubkey::Pubkey,
+    pub fee_claimer: solana_pubkey::Pubkey,
+    pub leftover_receiver: solana_pubkey::Pubkey,
+    pub config_parameters: ConfigParameters,
 }
